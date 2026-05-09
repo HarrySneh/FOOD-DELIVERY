7f132ae453875e53302acfeda8dd9373a26ea94a
Yunus <hmckaville@gmail.com>
2026-05-09 21:04:00 +0500

    Updates

---
+++ assets/images/imageConstants.ts
@@ -0,0 +1,59 @@
+// Logo
+export const LOGO = {
+  main: "/assets/images/logo/logo.svg",
+  white: "/assets/images/logo/logo-white.svg",
+  icon: "/assets/images/logo/icon.svg",
+};
+
+// Hero Images
+export const HERO_IMAGES = [
+  "/assets/images/hero/Chicken Breast.jpg",
+  "/assets/images/hero/Suya.jpg",
+  "/assets/images/hero/Meaty Baked Beans.jpg",
+];
+
+// Restaurant Images
+export const RESTAURANT_IMAGES = {  
+  tuoZaafiPalace: "/assets/images/restaurants/Egusi.jpg",
+  tamaleChopBar: "/assets/images/restaurants/Fried Plantain.jpg",
+  northernDelight: "/assets/images/restaurants/Ogbono.jpg",
+  savannaRestaurant: "/assets/images/restaurants/Ofada Sauce.jpg",
+  zongoJunction:
+    "/assets/images/restaurants/Red Stew with Assorted Meat.jpg",
+  dagbaniKitchen: "/assets/images/restaurants/Salmon Nuggets.jpg",
+  tastyJollof:
+    "/assets/images/restaurants/Spicy Cabbage w Smoked Turkey.jpg",
+  bankuParadise:
+    "/assets/images/restaurants/Seasoned Vegetables Medley.jpg",
+  waakyeHouse: "/assets/images/restaurants/One_Pan_Patato_and_Eggs.jpg",
+  kumasiFufuSpot: "/assets/images/restaurants/Honey Suya Wings.jpg",
+  gardenCityEats: "/assets/images/restaurants/Classic Caesar Salad.jpg",
+};
+
+// Dish Images
+export const DISH_IMAGES = {
+  jollofRice:   "/assets/images/dishes/Jollof.png",
+  bankuTilapia: "/assets/images/dishes/Banku.png",
+  waakye: "/assets/images/dishes/Waakye.png",
+  kelewele: "/assets/images/dishes/Kelewele.png",
+  fufu: "/assets/images/dishes/Fufu.png",
+  tuoZaafi: "/assets/images/dishes/Tuozaafi.png",
+  wasawasa: "/assets/images/dishes/Wasawasa.png",
+  kinkheba: "/assets/images/dishes/Kinkheba.png",
+  freshVegetables: "/assets/images/dishes/Vegitables.png",
+  riceBag: "/assets/images/dishes/Rice.png",
+  cookingOil: "/assets/images/dishes/Oil.png",
+};
+
+// Icon Images
+export const ICON_IMAGES = {
+  appStore: "/assets/images/icons/app-store.png",
+  playStore: "/assets/images/icons/play-store.png",
+  step1: "/assets/images/icons/step1.svg",
+  step2: "/assets/images/icons/step2.png",
+  step3: "/assets/images/icons/step3.png",
+  phoneMockup: "/assets/images/icons/phone-mockup.png",
+};
+
+// Default placeholder image
+export const PLACEHOLDER_IMAGE = "/src/assets/images/placeholder.jpg";
